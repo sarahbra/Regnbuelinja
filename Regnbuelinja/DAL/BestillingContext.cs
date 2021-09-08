b48f22b5fb5f0236bc4731cca43ec42b267b4b83
sarah <sarah@LAPTOP-BIUP68RE>
2021-09-08 11:17:51 +0200

    Starta på mappestruktur

---
+++ Regnbuelinja/DAL/BestillingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Regnbuelinja.DAL
+{
+    public class BestillingContext : DbContext
+    {
+        
+    }
+}
