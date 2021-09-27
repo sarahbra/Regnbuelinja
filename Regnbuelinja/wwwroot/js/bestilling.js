ccf59015a97dd23b2dcf978cf7a347353000ceb8
sarah <sarah@LAPTOP-BIUP68RE>
2021-09-27 18:40:00 +0200

    Prøver å få bestilling ut til skjerm

--- Regnbuelinja/wwwroot/js/bestilling.js
+++ Regnbuelinja/wwwroot/js/bestilling.js
@@ -1,13 +1,14 @@
 ﻿$(function () {
     const id = window.location.search.substring(1);
     const url = "Bestilling/HentBestilling?" + id;
-    $.get(url, function (bestilling) {
-        formaterBestilling(bestilling);
+    $.get(url, function (bestillingInput) {
+        formaterBestilling(bestillingInput);
     });
 })
 
-function formaterBestilling(bestilling) {
-    ut = "Hei, vindu, se jeg finnes!";
-
-    $("#bestillingen").html(ut)
+function formaterBestilling(bestillingInput) {
+    $("#utreiseHeader").val("Utreise fra " + bestillingInput.startpunkt);
+    $("#dato").val(bestillingInput.avreiseDato);
+    $("#skip").val("Båtten Anna");  //TODO - Add båtnavn
+    $("#strekning").val(bestillingInput.startpunkt + " - " + bestillingInput.endepunkt);
 }
