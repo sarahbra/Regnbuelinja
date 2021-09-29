95945854dd3ad27e6561e79f5545a14b8ea06225
vilderoxan <vilde.fauchald@gmail.com>
2021-09-29 22:26:19 +0200

    ikke ferdig kalender

---
+++ Regnbuelinja/wwwroot/js/formatdate.js
@@ -0,0 +1,33 @@
+﻿function formaterDato(datoer) {
+    let datoArray = [];
+    for (const dato of datoer) {
+        const formatYmd = dato => dato.toISOString().slice(0, 10);
+       
+         // adjust 0 before single digit date
+
+        let dag = ("0" + dato.getDate()).slice(-2);
+
+        // current month
+
+        let maaned = ("0" + (dato.getMonth() + 1)).slice(-2);
+
+        // current year
+
+        let aar = dato.getFullYear();
+
+        // current hours
+        let hours = date_ob.getHours();
+
+        // current minutes
+        let minutes = date_ob.getMinutes();
+
+        // current seconds
+        let seconds = date_ob.getSeconds();
+
+        // prints date & time in YYYY-MM-DD HH:MM:SS format
+
+        datoArray.push(dag + "/" + maaned + "/" + aar);
+        console.log(datoArray);
+    }
+    
+}
--- Regnbuelinja/wwwroot/js/index.js
+++ Regnbuelinja/wwwroot/js/index.js
@@ -1,135 +1,107 @@
-﻿
-$("input[type=radio][name=TurRetur]").change(function () {
-    var hjemreiseDato = $("#HjemreiseDato");
-    var tilbakeContainer = $("#TilbakeContainer");
-    if (this.value === "true") {
-        tilbakeContainer.removeClass("hidden");
-        hjemreiseDato.attr("required", true);
-    } else if (this.value === "false") {
-        tilbakeContainer.addClass("hidden");
-        hjemreiseDato.attr("required", false);
-    }
+﻿$("input[type=radio][name=TurRetur]").change(function () {
+  var hjemreiseDato = $("#HjemreiseDato");
+  var tilbakeContainer = $("#TilbakeContainer");
+  if (this.value === "true") {
+    tilbakeContainer.removeClass("hidden");
+    hjemreiseDato.attr("required", true);
+  } else if (this.value === "false") {
+    tilbakeContainer.addClass("hidden");
+    hjemreiseDato.attr("required", false);
+  }
 });
 
 $("#orderForm").submit(function (event) {
-    event.preventDefault();
-    var form = event.target;
-    var valid = form.checkValidity();
-    $(form).addClass('was-validated');
-    if (!valid) {
-        return false
-    }
-    $.post("Bestilling/LagreBestilling", $(this).serialize(), function (data) {
-        //console.log($(this))
-        window.location = "https://localhost:44392/bestilling.html?id=" + data;
-        //Gå til neste side med billettinfo
-    })
-        .fail(function () {
-            //Kan eventuelt hente ut feilmelding fra server her hvis vi vil etter at det er implementert
+  event.preventDefault();
+  var form = event.target;
+  var valid = form.checkValidity();
+  $(form).addClass("was-validated");
+  if (!valid) {
+    return false;
+  }
+  $.post("Bestilling/LagreBestilling", $(this).serialize(), function (data) {
+    //console.log($(this))
+    window.location = "https://localhost:44392/bestilling.html?id=" + data;
+    //Gå til neste side med billettinfo
+  }).fail(function () {
+    //Kan eventuelt hente ut feilmelding fra server her hvis vi vil etter at det er implementert
 
-            $("#feil").html("Feil på server - prøv igjen senere");
-        });
+    $("#feil").html("Feil på server - prøv igjen senere");
+  });
 });
 
 $("#Startpunkt").change(function () {
-    hentAnkomstHavner();
+  hentAnkomstHavner();
 });
 
 $("#Endepunkt").change(function () {
-    hentTilgjengeligeFerdDatoer();
+  hentTilgjengeligeFerdDatoer();
 });
 
-
 function hentAnkomstHavner() {
-    const avgangsHavn = $("#Startpunkt").val();
-    const url = "Bestilling/HentAnkomsthavner?avgangsHavn=" + avgangsHavn;
-    $.get(url, function (havner) {
-        visHavner($("#Endepunkt"), havner);
-        $("#EndepunktDiv").show();
-    });
+  const avgangsHavn = $("#Startpunkt").val();
+  const url = "Bestilling/HentAnkomsthavner?avgangsHavn=" + avgangsHavn;
+  $.get(url, function (havner) {
+    visHavner($("#Endepunkt"), havner);
+    $("#EndepunktDiv").show();
+  });
 }
 
 //Trenger ikke to sånne metoder som gjør det samme
 function visHavner(selectBox, havner) {
-    selectBox.empty();
-    selectBox.append('<option value="" disabled selected>Velg havn</option>');
-    for (let havn of havner) {
-        selectBox.append('<option value="' + havn + '">' + havn + '</option>');
-    }
+  selectBox.empty();
+  selectBox.append('<option value="" disabled selected>Velg havn</option>');
+  for (let havn of havner) {
+    selectBox.append('<option value="' + havn + '">' + havn + "</option>");
+  }
 }
 
 $.get("Bestilling/HentAvgangshavner", function (havner) {
-    visHavner($("#Startpunkt"), havner);
+  visHavner($("#Startpunkt"), havner);
 });
-
 
 //Hadde ikke trengt å sortere dato-strengene hvis vi hadde brukt Date backend. Da kunne vi brukt innebygde metoder på Date
 
 function visFerdKalender(datoer) {
-    function formaterDato(datoStreng) {
-        const deler = datoStreng.split(/[-T]+/).map(function (s) {
-            return parseInt(s)
-        });
-        console.log(datoStreng)
-        console.log(deler)
-        console.log(new Date(deler[0], deler[1] - 1, deler[2]));
-        return new Date(deler[0], deler[1] - 1, deler[2]);
-    }
-
-    const sorterteDatoer = datoer.map(formaterDato).sort(function (a, b) {
-        return a - b;
-    });
-    console.log(sorterteDatoer[0]);
-    console.log(sorterteDatoer[sorterteDatoer.length - 1]);
-
-    //Setter det samme på hjemreise og avreise. Sånn kan det ikke være
-    $("#AvreiseDato,#HjemreiseDato").datepicker({
-        format: "dd/mm/yyyy",
-        container: "body",
-        todayHighlight: true,
-        autoclose: true,
-        startDate: sorterteDatoer[0],//sorterteDatoer.slice(0).shift(),
-        endDate: sorterteDatoer[sorterteDatoer.length-1],//sorterteDatoer.slice(0).pop(),
-        beforeShowDay: function (date) {
-            const gyldig = sorterteDatoer.some(function (d) {
-                return d.getTime() === date.getTime();
-            })
-            return gyldig;
-        }
-    });
+  //Setter det samme på hjemreise og avreise. Sånn kan det ikke være
+  $("#AvreiseDato,#HjemreiseDato").datepicker({
+    format: "dd/mm/yyyy",
+    container: "body",
+    todayHighlight: true,
+    autoclose: true,
+    startDate: datoer[0],
+    endDate: datoer[datoer.length - 1],
+    beforeShowDay: function (date) {
+      const gyldig = sorterteDatoer.some(function (d) {
+        return d.getTime() === date.getTime();
+      });
+      return gyldig;
+    },
+  });
 }
-
 
 function hentTilgjengeligeFerdDatoer() {
-    const startPunkt = $("#Startpunkt").val();
-    const endePunkt = $("#Endepunkt").val();
+  const startPunkt = $("#Startpunkt").val();
+  const endePunkt = $("#Endepunkt").val();
 
-    $.get("Bestilling/HentRuter?nyttStartPunkt=" + startPunkt, function (startRuter) {
-        const ruter = startRuter.filter(function (startRute) {
-            return startRute.endepunkt === endePunkt;
-        });
-        Promise.all(ruter.map(function (rute) {
-            return $.get("Bestilling/HentFerder?ruteId=" + rute.rId);
-        })).then(function (results) {
-            const datoer = [];
-            for (const result of results) {
-                for (const ferd of result) {
-                    datoer.push(ferd.dato);
-                }
-            }
-            visFerdKalender(datoer);
-        });
+  let params = {
+    Startpunkt: startPunkt,
+    Endepunkt: endePunkt,
+    AvreiseDato: null,
+  };
+
+  console.log(params);
+
+  $.get("Bestilling/HentDatoer", params, function (datoer) {
+    console.log(datoer);
+    let datoFormat = [];
+    datoer.forEach(function (dato) {
+      datoFormat.push(formaterDato(dato));
     });
-
-    //Todo: Hvis tur/retur er valgt -> Hent datoer basert på startPunkt = endepunkt og endePunkt = startPunkt?
+    console.log(datoFormat);
+    //visFerdKalender(datoFormat);
+  });
+  //Todo: Hvis tur/retur er valgt -> Hent datoer basert på startPunkt = endepunkt og endePunkt = startPunkt?
 }
-
-
-
-
-
-
-
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
 /*
@@ -142,3 +114,18 @@ function hentTilgjengeligeFerdDatoer() {
 */
 
 //Serialiserer til formatet ovenfor. Slipper å hente ut data på "vanlig måte"
+
+//Gammel formaterDato da vi fikk String fra server
+
+/*
+    function formaterDato(datoStreng) {
+        const deler = datoStreng.split("/").map(function (s) {
+            return parseInt(s)
+        });
+        return new Date(deler[2], deler[1] - 1, deler[0]);
+    }
+
+    const sorterteDatoer = datoer.map(formaterDato).sort(function (a, b) {
+        return a - b;
+    });
+    */
