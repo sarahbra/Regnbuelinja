dff084ba0c022f73ad7225627f82ff4e6c8eddd8
vilderoxan <vilde.fauchald@gmail.com>
2021-11-02 19:55:26 +0100

    endrer regex-epost og gyldig json

--- Regnbuelinja/Models/Personer.cs
+++ Regnbuelinja/Models/Personer.cs
@@ -9,7 +9,7 @@ namespace Regnbuelinja.Models
         public string Fornavn { get; set; }
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,30}$")]
         public string Etternavn { get; set; }
-        [RegularExpression(@"^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9]+)*\\.([a-z]{2,4})$")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")]
         public string Epost { get; set; }
         [RegularExpression(@"^[0-9]{8}$")]
         public string Telefonnr { get; set; }
