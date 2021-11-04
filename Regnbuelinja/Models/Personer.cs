b6d85c610a5af6b6d62e010e64658c6e06794b6e
Degetei <daniel.gulliksen@gmail.com>
2021-11-04 21:40:49 +0100

    changed epost regex

--- Regnbuelinja/Models/Personer.cs
+++ Regnbuelinja/Models/Personer.cs
@@ -9,7 +9,7 @@ namespace Regnbuelinja.Models
         public string Fornavn { get; set; }
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ]{2,30}$")]
         public string Etternavn { get; set; }
-        [RegularExpression(@"/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/")]
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z")]
         public string Epost { get; set; }
         [RegularExpression(@"^[0-9]{8}$")]
         public string Telefonnr { get; set; }
