3d0c0e08881071a5ecb676a12edf794879b27d07
sarah <sarah@LAPTOP-BIUP68RE>
2021-11-04 20:51:02 +0100

    Regex-isoString kommentert ut

--- Regnbuelinja/Models/BestillingOutput.cs
+++ Regnbuelinja/Models/BestillingOutput.cs
@@ -9,9 +9,9 @@ namespace Regnbuelinja.Models
         public string Startpunkt { get; set; }
         [RegularExpression(@"/(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/")]
         public string Endepunkt { get; set; }
-        [RegularExpression(@"/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/")]
+        //[RegularExpression(@"/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/")]
         public string AvreiseTid { get; set; }
-        [RegularExpression(@"/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/")]
+        //[RegularExpression(@"/(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/")]
         public string HjemreiseTid { get; set; }
 
         public int AntallVoksne { get; set; }
--- Regnbuelinja/Models/FerdRute.cs
+++ Regnbuelinja/Models/FerdRute.cs
@@ -9,9 +9,9 @@ namespace Regnbuelinja.Models
     public class FerdRute
     {
         public int FId { get; set; }
-        [RegularExpression(@"/^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\d\d\d\d [012]{0,1}[0-9]:[0-6][0-9]$/")]
+        //[RegularExpression(@"/^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\d\d\d\d [012]{0,1}[0-9]:[0-6][0-9]$/")]
         public string AvreiseTid { get; set; }
-        [RegularExpression(@"/^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\d\d\d\d [012]{0,1}[0-9]:[0-6][0-9]$/")]
+        //[RegularExpression(@"/^([1-9]|([012][0-9])|(3[01])).([0]{0,1}[1-9]|1[012]).\d\d\d\d [012]{0,1}[0-9]:[0-6][0-9]$/")]
         public string AnkomstTid { get; set; }
 
         public string Strekning { get; set; }
